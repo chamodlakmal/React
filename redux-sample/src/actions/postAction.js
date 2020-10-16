a8e764b72dd957f4b403020b1bc82e7be95ad196
chamodlakmal <chamodlakmal97@gmail.com>
2020-10-16 10:07:29 +0530

    action added

---
+++ redux-sample/src/actions/postAction.js
@@ -0,0 +1,31 @@
+export const fetchPosts = () => {
+    return {
+        type: 'FETCH_BUTTON_CLICKED',
+        payload: [
+            {
+                userId: 1,
+                id: 1,
+                title: 'ssssss',
+                body: 'abc'
+            },
+            {
+                userId: 1,
+                id: 2,
+                title: 'ssssss',
+                body: 'abc'
+            },
+            {
+                userId: 1,
+                id: 3,
+                title: 'ssssss',
+                body: 'abc'
+            },
+            {
+                userId: 1,
+                id: 4,
+                title: 'ssssss',
+                body: 'abc'
+            }
+        ]
+    }
+}
